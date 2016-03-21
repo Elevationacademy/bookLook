50af4c88a0687c89c9b78b80a447d900fe03f87e
amhayslip <amhayslip@gmail.com>
2016-03-21 21:04:24 +0200

    setup

---
+++ script.js
@@ -0,0 +1,34 @@
+var books = [
+  { title: "Gilead", 
+  author: "Marilynne Robinson",
+  reviews: [
+    { name: "Aaron", review: "Bad" },
+    { name: "John", review: "Good" },
+    { name: "Terry", review: "Huh?" },
+    { name: "Yusuf", review: "O'doyle Rules!" }
+  ]},
+  { title: "Huck Finn", 
+  author: "Mark Twain",
+  reviews: [
+    { name: "Aaron", review: "Bad" },
+    { name: "John", review: "Good" },
+    { name: "Terry", review: "Huh?" },
+    { name: "Yusuf", review: "O'doyle Rules!" }
+  ]},
+  { title: "Harry Potter", 
+  author: "J.K. Rowling",
+  reviews: [
+    { name: "Aaron", review: "Bad" },
+    { name: "John", review: "Good" },
+    { name: "Terry", review: "Huh?" },
+    { name: "Yusuf", review: "O'doyle Rules!" }
+  ]},
+  { title: "Lean Startup", 
+  author: "Eric Ries",
+  reviews: [
+    { name: "Aaron", review: "Bad" },
+    { name: "John", review: "Good" },
+    { name: "Terry", review: "Huh?" },
+    { name: "Yusuf", review: "O'doyle Rules!" }
+  ]}
+];

75566110e51e046f37ea65d68d7f18907ce90e34
amhayslip <amhayslip@gmail.com>
2016-03-26 21:18:11 +0300

    fix t

--- script.js
+++ script.js
@@ -1,29 +1,21 @@
 var BookSearchApp = function () {
-  var createBook = function (title, author, description, image, pages) {
-    var book = {
-      title: title,
-      author: author,
-      description: description,
-      image: image,
-      pages: pages
-    }
+  var calculateReading = function (pages, minutes) {
+    var averagePagePerMinute = 1;
 
-    var source = $("#book-template").html();
-    var template = Handlebars.compile(source);
-    var context = book;
-    var html = template(book);
-
-    $('.book').html(html);
+    return averagePagePerMinute * pages / minutes;
   }
 
-  var validateEntry = function (title, author, description, image, pages) {
+  var validateEntry = function (title, author, description, image, pages, minutes) {
     var errors = [];
 
     if (title.length < 1) errors.push('title');
     if (author.length < 1) errors.push('author');
     if (description.length < 1) errors.push('description');
     if (image.length < 1) errors.push('image');
-    if (pages.length < 1) errors.push('pages');
+    if (typeof pages !== "number" || !pages) errors.push('pages');
+    if (typeof minutes !== "number" || !minutes) errors.push('minutes');
+
+    console.log(errors);
 
     if (errors.length > 0) {
       for (var i = 0; i < errors.length; i += 1) {
@@ -32,8 +24,29 @@ var BookSearchApp = function () {
     } else {
       $('.errors-container').children().removeClass('show');
 
-      createBook(title, author, description, image, pages);
+      var timeToFinish = calculateReading(pages, minutes);
+
+      createBook(title, author, description, image, pages, minutes, timeToFinish);
     }
+  }
+
+  var createBook = function (title, author, description, image, pages, minutes, timeToFinish) {
+    var book = {
+      title: title,
+      author: author,
+      description: description,
+      image: image,
+      pages: pages,
+      minutes: minutes,
+      timeToFinish: timeToFinish
+    }
+
+    var source = $("#book-template").html();
+    var template = Handlebars.compile(source);
+    var context = book;
+    var html = template(book);
+
+    $('.book').html(html);
   }
 
   return {
@@ -50,7 +63,8 @@ $('.search-book').on('click', function (e) {
   var author = $('#author').val();
   var description = $('#description').val();
   var image = $('#image').val();
-  var pages = $('#pages').val();
+  var pages = parseInt($('#pages').val(), 10);
+  var minutes = parseInt($('#minutes').val(), 10)
 
-  app.validateEntry(title, author, description, image, pages);
+  app.validateEntry(title, author, description, image, pages, minutes);
 });

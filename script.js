46320a7db57e35e5153535cbeb01082f6ba56bd2
amhayslip <amhayslip@gmail.com>
2016-03-26 13:44:23 +0300

    add API for isbn

--- script.js
+++ script.js
@@ -1,34 +1,53 @@
-var books = [
-  { title: "Gilead", 
-  author: "Marilynne Robinson",
-  reviews: [
-    { name: "Aaron", review: "Bad" },
-    { name: "John", review: "Good" },
-    { name: "Terry", review: "Huh?" },
-    { name: "Yusuf", review: "O'doyle Rules!" }
-  ]},
-  { title: "Huck Finn", 
-  author: "Mark Twain",
-  reviews: [
-    { name: "Aaron", review: "Bad" },
-    { name: "John", review: "Good" },
-    { name: "Terry", review: "Huh?" },
-    { name: "Yusuf", review: "O'doyle Rules!" }
-  ]},
-  { title: "Harry Potter", 
-  author: "J.K. Rowling",
-  reviews: [
-    { name: "Aaron", review: "Bad" },
-    { name: "John", review: "Good" },
-    { name: "Terry", review: "Huh?" },
-    { name: "Yusuf", review: "O'doyle Rules!" }
-  ]},
-  { title: "Lean Startup", 
-  author: "Eric Ries",
-  reviews: [
-    { name: "Aaron", review: "Bad" },
-    { name: "John", review: "Good" },
-    { name: "Terry", review: "Huh?" },
-    { name: "Yusuf", review: "O'doyle Rules!" }
-  ]}
-];
+var BookSearchApp = function () {
+  var book = null;
+
+  var createBook = function (data) {
+    if (data.totalItems) {
+      var book = {
+        title: data.items[0].volumeInfo.title,
+        author: data.items[0].volumeInfo.authors[0],
+        image: data.items[0].volumeInfo.imageLinks["thumbnail"],
+      }
+
+      var source = $("#book-template").html();
+      var template = Handlebars.compile(source);
+      var context = book;
+      var html = template(book);
+
+      $('.book').html(html);
+   
+    }
+  }
+
+  var fetch = function (isbn) {
+    $.ajax({
+      method: "GET",
+      url: 'https://www.googleapis.com/books/v1/volumes?q=' + isbn,
+      dataType: "jsonp",
+      jsonp: "callback",
+      success: function(data) {
+        console.log('success!');
+        createBook(data);
+      },
+      error: function(jqXHR, textStatus, errorThrown) {
+        console.log(textStatus);
+      }
+    }); 
+  }
+
+  return {
+    fetch: fetch
+  }
+}
+
+var app = BookSearchApp();
+
+$('.search-book').on('click', function (e) {
+  e.preventDefault();
+
+  var isbn = $('#isbn-number').val();
+  $('#isbn-number').val('');
+
+
+  app.fetch(isbn)
+});
